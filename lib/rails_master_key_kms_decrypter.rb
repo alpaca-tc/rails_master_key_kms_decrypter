2d63e79963915b896c7718d6a6f0e4f1f4f776e9
alpaca-tc <alpaca-tc@alpaca.tc>
2018-02-17 05:18:17 +0900

    Initialize repository

---
+++ lib/rails_master_key_kms_decrypter.rb
@@ -0,0 +1,5 @@
+require "rails_master_key_kms_decrypter/version"
+
+module RailsMasterKeyKmsDecrypter
+  # Your code goes here...
+end
---
+++ lib/rails_master_key_kms_decrypter/version.rb
@@ -0,0 +1,3 @@
+module RailsMasterKeyKmsDecrypter
+  VERSION = "0.1.0"
+end
